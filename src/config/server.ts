e2e3cc0e4e173940ac8c32178b614bf108417280
ic3top <dvdvdv889@gmail.com>
2022-10-02 01:50:13 +0300

    Add categories router

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import cors from 'cors';
 
 import { errorMiddleware } from '../middleware/error.middleware';
+import { loggerMiddleware } from '../utils/logger';
 
 import usersRouter from '../controllers/users.controller';
-import { loggerMiddleware } from '../utils/logger';
+import categoriesRouter from '../controllers/categories.controller';
 
 function createServer() {
   const app = express();
@@ -14,6 +15,7 @@ function createServer() {
   app.use(loggerMiddleware);
 
   app.use('/api/users', usersRouter);
+  app.use('/api/categories', categoriesRouter);
 
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
